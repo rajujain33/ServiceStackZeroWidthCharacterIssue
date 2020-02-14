fa7e91c22b544090a0cf7485f88d9ff550a6e1de
Raju Jain <rajujain@kghcustoms.com>
2020-02-14 10:32:38 +0100

    Sample code to replicate issue

---
+++ WebApplication1/WebApplication1/WebApplication1.ServiceInterface/MyServices.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack;
+using WebApplication1.ServiceModel;
+
+namespace WebApplication1.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Post(Hello request)
+        {
+            string str = request.Name as string;
+            char s = str[0];
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/WebApplication1.ServiceModel/Hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+
+namespace WebApplication1.ServiceModel
+{
+    [Route("/hello")]
+    public class Hello : IReturn<bool>
+    {
+        public object Name { get; set; }
+    }
+}
